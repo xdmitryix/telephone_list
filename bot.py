4d11a80cae7fb3ed59b6f7d544b0e878e0d6bf2e
dmitry <rbdeman@yandex.ru>
2023-02-22 22:36:09 +0300

    добавил команды

--- bot.py
+++ bot.py
@@ -3,6 +3,17 @@ import json
 telephone_sp = {'Иванов Иван Иванович': '89274678934', 'Петров Петр Петрович': '83427773432', 'Федоров Федор Федорович': '82227553678'}
 
 
+def add_list(data_list):
+    it = iter(input('Введите ФИО и номер телефона через ":" ').split(':'))
+    data_list.update(dict(zip(it, it)))
+
+def delete_list(data_list):
+    d = input('Введите ФИО удаляемого контакта: ')
+    if d in data_list:
+        del data_list[d]
+
+
+
 
 print('Добро пожаловать в телефонный справочник!')
 while True:
@@ -11,16 +22,25 @@ while True:
         print('Бот начал работу !')
     elif command == '/help':
         print ('Введи команду:\n /help - справка\n /start - начало чата\n /quit - выход\n /add - добавить контакт\n /delete - удалить контакт\n /stop - остановить бот\n \
-/all - выведи весь список контактов\n')
+/all - выведи весь список контактов\n /add - добавить новый контакт\n /del - удалить контакт из справочника ')
     elif command == '/stop':
         print('Бот остановил свою работу.')
         break
     elif command == '/all':
         print('Весь список контактов:')
         print(telephone_sp)
-    elif command == '/find':
-        find_data = input('Введите искомые данные:')
-        for k,v in telephone_sp.items():
-            if find_data in telephone_sp[k,v]:
-                print(k,v)
+    elif command == '/add':
+        add_list(telephone_sp)
+        print('контакт успешно добавлен в справочник!')
+    elif command == '/del':
+        delete_list(telephone_sp)
+        print('контакт успешно удален из справочника!')
+
+
+
+    # elif command == '/find':
+    #     find_data = input('Введите искомые данные:')
+    #     for k,v in telephone_sp.items():
+    #         if find_data in telephone_sp[k,v]:
+    #             print(k,v)
             
