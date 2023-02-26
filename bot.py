29971a128f6db1f9c31956eb3654d36b6820f817
dmitry <rbdeman@yandex.ru>
2023-02-26 15:03:08 +0300

    добавлена функция редактирования данных

--- bot.py
+++ bot.py
@@ -1,17 +1,19 @@
 import json
 
+# изначальный список:
 telephone_sp= [
                 {'фамилия': 'Петров', 'имя': 'Петр', 'отчество': 'Петрович', 'телефон': '89567378844'},
                 {'фамилия': 'Иванов', 'имя': 'Иван', 'отчество': 'Иванович', 'телефон': '89445678900'},
                 {'фамилия': 'Васильев', 'имя': 'Василий', 'отчество': 'Васильевич', 'телефон': '89564468909'},
                 ]
 
-
+# функция сохранения данных:
 def save():
     with open("telephone_sp.json","w", encoding="utf-8") as ts:
         ts.write(json.dumps(telephone_sp,ensure_ascii=False))
     print("Ваш телефонный справочник был успешно сохранен в файле telephone_sp.json")
 
+# функция добавления данных в список:
 def add_list(data_list):
     f = input('введите фамилию нового контакта: ')
     i = input('введите имя нового контакта: ')
@@ -19,6 +21,7 @@ def add_list(data_list):
     t = input('введите номер телефона нового контакта: ')
     data_list.append({'фамилия': f, 'имя': i, 'отчество': o, 'телефон': t})
 
+# функция удаления данных из списка:
 def delete_list(data_list):
     res = []
     text = input('введите данные удаляемого контакта: ')
@@ -51,8 +54,35 @@ def delete_list(data_list):
                 del data_list[temp_nums]
                 print('контакт успешно удален из списка !')
             temp_nums = temp_nums+1
-            
 
+# функция редактирования данных в списке:
+def redact(data_list):
+    text_mes = input('введите данные изменяемого контакта(фамилию или имя или отчество или телефон): ')
+    parametr = input('какой параметр нужно изменить? (фамилия/имя/отчество/телефон): ')
+    if parametr == 'фамилия':
+        for i in data_list:  
+            if i['фамилия'] == text_mes:
+                new = input('введите новую фамилию для контакта:')
+                i['фамилия'] = new
+    elif parametr == 'имя':
+        for i in data_list:  
+            if i['имя'] == text_mes:
+                new = input('введите новое имя для контакта:')
+                i['имя'] = new 
+    elif parametr == 'отчество':
+        for i in data_list:  
+            if i['отчество'] == text_mes:
+                new = input('введите новое отчество для контакта:')
+                i['отчество'] = new   
+    elif parametr == 'телефон':
+        for i in data_list:  
+            if i['телефон'] == text_mes:
+                new = input('введите новый телефон для контакта:')
+                i['телефон'] = new 
+    else:
+        print('некорректный ввод.')        
+
+# функция поиска данных в списке:
 def search_list(data_list):
     res = []
     text = input('введите данные искомого контакта: ')
@@ -74,7 +104,7 @@ def search_list(data_list):
         print('контакт отсутствует в списке.')
 
 
-
+# сама программа:
 
 print('Добро пожаловать в телефонный справочник! Введите /help для просмотра мануала')
 while True:
@@ -84,7 +114,7 @@ while True:
     elif command == '/help':
         print ('Введи команду:\n /help - справка\n /start - начало чата\n /stop - остановить бот\n \
 /all - выведи весь список контактов\n /add - добавить новый контакт\n /del - удалить контакт из справочника\n /save - сохранение данных\n /load - загрузка данных\n \
-/sear - поиск контакта\n')
+/sear - поиск контакта\n /red - изменить данные контакта\n')
     elif command == '/stop':
         save()
         print('Бот остановил свою работу.')
@@ -105,14 +135,7 @@ while True:
         print("Ваш телефонный справочник был успешно загружен из файла telephone_sp.json")
     elif command == '/sear':
         search_list(telephone_sp)
+    elif command == '/red':
+        redact(telephone_sp)
     else:
         print("Неопознанная команда. Просьба изучить мануал через команду /help")
-
-
-
-    # elif command == '/find':
-    #     find_data = input('Введите искомые данные:')
-    #     for k,v in telephone_sp.items():
-    #         if find_data in telephone_sp[k,v]:
-    #             print(k,v)
-            
