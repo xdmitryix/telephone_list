1e3b51010d9c0cf9b9e4e55ccf0536fac62f5aed
dmitry <rbdeman@yandex.ru>
2023-02-26 13:01:02 +0300

    добавил команды search и del

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import json
 
-telephone_sp = {}
-telephone_sp["Иванов Иван Иванович"] = '89274678934'
-telephone_sp["Петров Петр Петрови"] = '83427773432'
-telephone_sp["Федоров Федор Федорович"] = '82227553678'
+telephone_sp= [
+                {'фамилия': 'Петров', 'имя': 'Петр', 'отчество': 'Петрович', 'телефон': '89567378844'},
+                {'фамилия': 'Иванов', 'имя': 'Иван', 'отчество': 'Иванович', 'телефон': '89445678900'},
+                {'фамилия': 'Васильев', 'имя': 'Василий', 'отчество': 'Васильевич', 'телефон': '89564468909'},
+                ]
 
 
 def save():
@@ -12,16 +13,65 @@ def save():
     print("Ваш телефонный справочник был успешно сохранен в файле telephone_sp.json")
 
 def add_list(data_list):
-    it = iter(input('Введите ФИО и номер телефона через ":" ').split(':'))
-    data_list.update(dict(zip(it, it)))
+    f = input('введите фамилию нового контакта: ')
+    i = input('введите имя нового контакта: ')
+    o = input('введите отчество нового контакта: ')
+    t = input('введите номер телефона нового контакта: ')
+    data_list.append({'фамилия': f, 'имя': i, 'отчество': o, 'телефон': t})
 
 def delete_list(data_list):
-    d = input('Введите ФИО удаляемого контакта: ')
-    if d in data_list:
-        del data_list[d]
-        print('контакт успешно удален из справочника!')
+    res = []
+    text = input('введите данные удаляемого контакта: ')
+    for i in data_list:  
+        if i['фамилия'] == text:
+            res.append(i)
+        elif i['имя'] == text:
+            res.append(i)
+        elif i['отчество'] == text:
+            res.append(i)
+        elif i['телефон'] == text:
+            res.append(i)
+    if len(res) < 1:
+        print('пользователя нет в списке контактов.')
+    elif len(res) == 1:
+        temp = 0 
+        for k in data_list:
+            for l in res:
+                if k == l:
+                    del data_list[temp]
+                temp = temp + 1 
+        print('Контакт успешно удален из справочника!')
     else:
-        print('контакт не найден в списке.')
+        temp_nums = 0
+        print('В справочнике нашлось', len(res) , 'котнакта подхожящих под кретерий поиска:')
+        print(res)
+        num = input('Для корректного удаления нужного контакта воспользуйтесь поиском по номеру телефона! Введите номер удаляемого контакта: ')
+        for m in data_list:
+            if m['телефон'] == num:
+                del data_list[temp_nums]
+                print('контакт успешно удален из списка !')
+            temp_nums = temp_nums+1
+            
+
+def search_list(data_list):
+    res = []
+    text = input('введите данные искомого контакта: ')
+    flag = False
+    for i in data_list:     
+        if i['фамилия'] == text:
+            print(i)
+            flag = True
+        elif i['имя'] == text:
+            print(i)
+            flag = True 
+        elif i['отчество'] == text:
+            print(i)
+            flag = True 
+        elif i['телефон'] == text:
+            print(i)
+            flag = True 
+    if flag == False:
+        print('контакт отсутствует в списке.')
 
 
 
@@ -33,7 +83,8 @@ while True:
         print('Бот начал работу !')
     elif command == '/help':
         print ('Введи команду:\n /help - справка\n /start - начало чата\n /stop - остановить бот\n \
-/all - выведи весь список контактов\n /add - добавить новый контакт\n /del - удалить контакт из справочника\n /save - сохранение данных\n /load - загрузка данных\n ')
+/all - выведи весь список контактов\n /add - добавить новый контакт\n /del - удалить контакт из справочника\n /save - сохранение данных\n /load - загрузка данных\n \
+/sear - поиск контакта\n')
     elif command == '/stop':
         save()
         print('Бот остановил свою работу.')
@@ -52,6 +103,8 @@ while True:
         with open("telephone_sp.json","r", encoding="utf-8") as ts:
             telephone_sp = json.load(ts)
         print("Ваш телефонный справочник был успешно загружен из файла telephone_sp.json")
+    elif command == '/sear':
+        search_list(telephone_sp)
     else:
         print("Неопознанная команда. Просьба изучить мануал через команду /help")
 
